0201c849fa12b0f62d82ae828b409a9edd397f98
Sanjayveeraswamy <sanjaykota503@gmail.com>
2026-02-16 22:35:57 +0530

    Initial commit

---
+++ day 1/variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'sanjay'    // first name of a person
+let lastName = 'veera swamy'      // last name of a person
+let country = 'india'       // country
+let city = 'bapatla'         // capital city
+let age = 21                // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'sanjay',     //name of a person
+  job = 'student',
+  live = 'india'
